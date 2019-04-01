6de7d5e99143f39dcb62545840be907b787c3706
guangge1977 <guangge1977@189.cn>
2019-04-01 17:32:00 +0800

    可以导入下级目录了。

--- __init__.py
+++ __init__.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     cherrypy.quickstart(Root,config=conf)
     sys.exit()
 
-
 class default(object):
     exposed = True
     _cls_cnt = 0
@@ -30,4 +29,10 @@ class default(object):
         self.__class__._cls_cnt -= 1
         pass
 
-
+if __name__ not in ('__main__', '_mp__main__'):
+    import os
+    print(__name__, os.getcwd())
+    #print(__name__, {}, {}, [x for x in os.listdir(__name__, replace('.', os
+    # 尝试导入所有下级目录， 会自动映射为下级URL。
+    __import__(__name__, {}, {}, [x for x in os.listdir(__name__.replace('.', os.sep)) if x.count('.')<1])
+    #def x
